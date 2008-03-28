a5eb4650d937a172d8965bb5a591b9677216713d
saruskas <saruskas@28b0a3f2-3848-0410-a3f5-bf879e04881c>
2008-03-28 13:38:06 +0000

    interfazGráfica(esqueleto)

---
+++ trunk/ontologyTestingGUI/src/ontologytestinggui/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ontologytestinggui;
+
+/**
+ *
+ * @author sara_garcia
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
