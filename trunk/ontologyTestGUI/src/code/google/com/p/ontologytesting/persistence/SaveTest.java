4b3035585a8dfbab2c8958530884776b0945a12b
saruskas <saruskas@28b0a3f2-3848-0410-a3f5-bf879e04881c>
2008-09-22 11:39:48 +0000

    Nueva versión del proyecto con la nueva interfaz

---
+++ trunk/ontologyTestGUI/src/code/google/com/p/ontologytesting/persistence/SaveTest.java
@@ -0,0 +1,105 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package code.google.com.p.ontologytesting.persistence;
+
+import code.google.com.p.ontologytesting.guiNew.MainApplication;
+import code.google.com.p.ontologytesting.model.CollectionTest;
+import code.google.com.p.ontologytesting.model.Instancias;
+import code.google.com.p.ontologytesting.model.ScenarioTest;
+import java.awt.Component;
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author saruskas
+ */
+public class SaveTest {
+    private Component frame;
+    private CollectionTest collection;
+    
+    public void saveProject() throws FileNotFoundException{
+        collection = MainApplication.getCollection();
+        //String onto = collection.getOntology();
+        //String[] o = onto.split("\\\\");
+        //String ontoFinal = "./"+o[o.length-1];
+        //collection.setOntology(ontoFinal);
+        XMLEncoder e = new XMLEncoder(new BufferedOutputStream(new 
+        FileOutputStream(MainApplication.getProyecto()+"/"+MainApplication.getNombreProyecto())));
+        e.writeObject(collection);
+        e.close();
+    }
+    
+    public boolean saveProjectAs() throws FileNotFoundException{
+        collection = MainApplication.getCollection();
+        JFileChooser fileChooser = new JFileChooser();
+        int seleccion = fileChooser.showSaveDialog(frame);
+        if(seleccion == JFileChooser.APPROVE_OPTION){
+            File fichero = fileChooser.getSelectedFile();
+            XMLEncoder e = new XMLEncoder(new BufferedOutputStream(new 
+            FileOutputStream(fichero)));
+            e.writeObject(collection);
+            e.close();
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean saveTestLocally(ScenarioTest scenarioTest){
+        ArrayList<ScenarioTest> scenario = MainApplication.getCollection().getScenariotest();
+        if(scenario.add(scenarioTest)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean replaceTestLocally(ScenarioTest scenarioTest){
+        ArrayList<ScenarioTest> scenario = MainApplication.getCollection().getScenariotest();
+        for(int i=0; i<scenario.size(); i++){
+            if(scenarioTest.getNombre().equals(scenario.get(i).getNombre())){
+                scenario.remove(i);
+                scenario.add(scenarioTest);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void saveTestInMemory(ScenarioTest scenarioTest) throws FileNotFoundException{
+        XMLEncoder e = new XMLEncoder(new BufferedOutputStream(new 
+        FileOutputStream(MainApplication.getProyecto()+"/"+scenarioTest.getNombre())));
+        e.writeObject(scenarioTest);
+        e.close();
+    }
+    
+    public void saveInstanciasInMemory(Instancias instancias){
+        try{ 
+            if(!instancias.getNombre().equals("")){
+                XMLEncoder e = new XMLEncoder(new BufferedOutputStream(new 
+                FileOutputStream(MainApplication.getProyecto()+"/"+instancias.getNombre())));
+                e.writeObject(instancias);
+                e.close();
+                JOptionPane.showMessageDialog(frame,"Instancias guardadas con el " +
+                        "nombre que les asigno y asociadas al test",
+                        "Information Message",JOptionPane.INFORMATION_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(frame,"Debe introducir un nombre para el " +
+                        "conjunto de instancias.","Warning Message",JOptionPane.WARNING_MESSAGE);
+            }
+        }catch (FileNotFoundException ex) {
+           JOptionPane.showMessageDialog(frame,"No se ha podido guardar el archivo","Error Message",JOptionPane.ERROR_MESSAGE);
+        } 
+    }
+
+}
